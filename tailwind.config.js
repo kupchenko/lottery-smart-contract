e3b4d9af89e72fad19eed0147c8f88135c724190
Dmitrii Kupchenko <kupchenko.work@gmail.com>
2024-06-19 17:59:08 +0400

    Add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      boxShadow: {
+        'allsides': '0 5px 5px -3px rgb(0 0 0 / 20%), 0px 8px 10px 1px rgb(0 0 0 / 14%), 0px 3px 14px 2px rgb(0 0 0 / 12%)'
+      }
+    },
+  },
+  plugins: [],
+}
+
